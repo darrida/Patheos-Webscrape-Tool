87d1d98d1c07aa9a788d4d5d85bcbe3f50412aa6
darrida <darrida.py@gmail.com>
2020-03-19 22:00:29 -0500

    sql lite db tests finished

--- app_files_v1/tests/test_db.py
+++ app_files_v1/tests/test_db.py
@@ -52,8 +52,12 @@ def test_insert_website(connection=None):
     with data.database(connection) as db:
         w = data.website(name='Patheos Blogs', url='https://www.patheos.com/blogs')
         db.insert_website(w)
+        w = data.website(name='Google', url='https://www.google.com')
+        db.insert_website(w)
         result = db.execute('SELECT url FROM websites WHERE name = \'Patheos Blogs\'')
+        result_2 = db.execute('SELECT id FROM websites WHERE name = \'Google\'')
     assert result[0][0] == 'https://www.patheos.com/blogs'
+    assert result_2[0][0] == 2
 
 
 def test_insert_category(connection=None):
@@ -66,8 +70,15 @@ def test_insert_category(connection=None):
                           url='https://www.patheos.com/buddhist-blogs',
                           website_id=1)
         db.insert_category(c)
+        c = data.category(name='Google Blogs',
+                          context='Context',
+                          url='https://www.google.com/blog-something',
+                          website_id=2)
+        db.insert_category(c)
         result = db.execute('SELECT name FROM categories WHERE name = \'Buddhist Blogs\'')
+        result_2 = db.execute('SELECT id FROM categories WHERE name = \'Google Blogs\'')
     assert result[0][0] == 'Buddhist Blogs'
+    assert result_2[0][0] == 2
 
 
 def test_insert_blog(connection=None):
@@ -80,8 +91,15 @@ def test_insert_blog(connection=None):
                       url='https://www.patheos.com/blogs/americanbuddhist/',
                       category_id=1)
         db.insert_blog(b)
+        b = data.blog(author='darrida',
+                      name='American Google',
+                      url='https://www.google.com/blogs/americangoogle/',
+                      category_id=2)
+        db.insert_blog(b)
         result = db.execute('SELECT url FROM blogs WHERE name = \'American Buddhist\'')
+        result_2 = db.execute('SELECT id FROM blogs WHERE name = \'American Google\'')
     assert result[0][0] == 'https://www.patheos.com/blogs/americanbuddhist/'
+    assert result_2[0][0] == 2
 
 
 def test_insert_post(connection=None):
@@ -97,8 +115,29 @@ def test_insert_post(connection=None):
                       url='https://www.patheos.com/blogs/americanbuddhist/2020/01/shambhala-buddhist-community-faces-new-allegations-in-chapman-student-investigation.html',
                       blog_id=1)
         db.insert_post(p)
+        p = data.post(title='Google post about things',
+                      author='first last',
+                      date='JANUARY 4, 2020',
+                      tags='tag1, tag2',
+                      content='In a video that should be seen by Google students and Google professors...',
+                      url='https://www.google.com/blog/american-google.html',
+                      blog_id=2)
+        db.insert_post(p)
         result = db.execute('SELECT author FROM posts WHERE title = \'Shambhala Buddhist Community Faces New Allegations in Chapman Student Investigation\'')
+        result_2 = db.execute('SELECT id FROM posts WHERE title = \'Google post about things\'')
     assert result[0][0] == 'JUSTIN WHITAKER'
+    assert result_2[0][0] == 2
+
+
+def test_query_websites(connection=None):
+    if connection == None:
+        __DB_LOCATION = Path.cwd() / 'tests' / 'test_files' / 'test.db'
+        connection = sqlite3.connect(str(__DB_LOCATION))
+    with data.database(connection) as db:
+        result = db.query_websites('Patheos Blogs')
+    assert result.name == 'Patheos Blogs'
+    assert result.url == 'https://www.patheos.com/blogs'
+    assert result.id == 1
 
 
 def test_teardown_install_files():
